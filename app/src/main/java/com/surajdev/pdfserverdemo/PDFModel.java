936e28dd5e180f86605a9dd781f12b2352e29294
Suraaj <sdevgan007@gmail.com>
2021-02-15 14:41:10 +0530

    Initial commit

---
+++ app/src/main/java/com/surajdev/pdfserverdemo/PDFModel.java
@@ -0,0 +1,39 @@
+package com.surajdev.pdfserverdemo;
+
+
+
+public class PDFModel {
+
+    private String pdfName;
+    private String pdfUrl;
+
+
+    public PDFModel(String pdfName, String pdfUrl) {
+        this.pdfName = pdfName;
+        this.pdfUrl = pdfUrl;
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public void setPdfName(String pdfName) {
+        this.pdfName = pdfName;
+    }
+
+    public String getPdfUrl() {
+        return pdfUrl;
+    }
+
+    public void setPdfUrl(String pdfUrl) {
+        this.pdfUrl = pdfUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "PDFModel{" +
+                "pdfName='" + pdfName + '\'' +
+                ", pdfUrl='" + pdfUrl + '\'' +
+                '}';
+    }
+}
